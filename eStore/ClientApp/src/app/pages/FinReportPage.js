34913113841fad17d0d82d81d3989ad5d6973aa7
Amit Kumar <amitnarayansah@gmail.com>
2021-08-05 20:58:34 +0530

    fin page some changes

--- eStore/ClientApp/src/app/pages/FinReportPage.js
+++ eStore/ClientApp/src/app/pages/FinReportPage.js
@@ -12,144 +12,12 @@ import {
   Card,
   CardBody,
   CardHeader,
-  CardHeaderToolbar,
-  Notice,
+  CardHeaderToolbar
 } from "../../_metronic/_partials/controls";
-import { usePromiseTracker } from "react-promise-tracker";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-
 import * as EmpAcction from "../modules/Payrolls/_redux/employees/Actions";
-
 import axios from "axios";
 import { BASE_URL } from "../../_estore/URLConstants";
-
-export const FinReportPage = () => {
-  const suhbeader = useSubheader();
-  suhbeader.setTitle("Fin Year Report");
-
-  return (
-    <>
-     
-     <Table>
-       <TableRow>
-       <TableCell> <RequestCard /></TableCell>
-       <TableCell><AttendaceReportCard/></TableCell>
-      </TableRow>
-      </Table>
-    </>
-  );
-};
-
-export const RequestCard = () => {
-  const curYear = new Date().getFullYear() + 1;
-  const [yearArray, setYearArray] = useState([]);
-  const [finYear, setFinYear] = useState("");
-  const [repoMode, setRepoMode] = useState(2);
-  const [store, setStore] = useState(1);
-  const [refreshData,setRefreshData] = useState(false);
-  let count = 0;
-  for (let index = 2015; index < curYear; index++) {
-    yearArray[count++] = "" + index + "-" + (index + 1);
-  }
-  const handleYearChange = (event) => {
-    setFinYear(event.target.value);
-    console.log(finYear); 
-    console.log(event.target.value);
-  };
-  const handleModeChange = (event) => {
-    setRepoMode(event.target.value);
-  };
-  const handleRefreshChange = (event) => {
-    setRefreshData(event.target.checked);
-  };
-  const handleStoreChange = (event) => {
-    setStore(event.target.value);
-  };
-  const handleButton = (event) => {
-    const yrs = finYear.split("-");
-    console.log(yrs);
-    const finReq = {
-        storeId: store,
-        startYead: parseInt(yrs[0]),
-      endYear: parseInt(yrs[1]),
-      startMonth: 0,
-      endMonth: 0,
-      mode: parseInt(repoMode),
-      forcedRefresh: refreshData, 
-      isPdf:true
-    };
-    console.log(finReq);
-    GetReport(finReq);
-  };
-
-  return (
-    <Card>
-      <CardHeader title="Financial  Year Report Download">
-        <CardHeaderToolbar></CardHeaderToolbar>
-      </CardHeader>
-      <CardBody>
-      {/* {loading ? <LoadingSpinner /> : <ResultsTable results={data} />} */}
-        <Table>
-          <TableRow>
-            <TableCell className="text-primary">Store</TableCell>
-            <TableCell>
-              <Select value={store} onChange={handleStoreChange} id="storeIdSelect">
-                <MenuItem value={1}>Dumka</MenuItem>
-                <MenuItem value={2}>Jamshedpur</MenuItem>
-              </Select>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="text-danger">Financial Year</TableCell>
-            <TableCell>
-              <Select value={finYear} displayEmpty onChange={handleYearChange} id="finYearSelect">
-              <MenuItem value="" disabled>Select Fin Year</MenuItem>
-                {yearArray.map((item) => (
-                  <MenuItem key={item} value={item}>{item}</MenuItem>
-                ))}
-              </Select>
-            </TableCell>
-            <TableCell className="text-info">Report</TableCell>
-            <TableCell>
-              <Select value={repoMode} onChange={handleModeChange} id="modeSelect">
-                <MenuItem value={1}>Sale Data</MenuItem>
-                <MenuItem value={3}>Salary Data</MenuItem>
-                <MenuItem value={2}>Cash Book</MenuItem>
-                <MenuItem value={4}>Expenses Data</MenuItem>
-                <MenuItem value={5}>Payment Data</MenuItem>
-                <MenuItem value={6}>Receipts Data</MenuItem>
-                <MenuItem value={7}>Bank Data</MenuItem>
-                <MenuItem value={8} disabled>Purchase Data</MenuItem>
-              </Select>
-            </TableCell>
-            <TableCell className="text-danger">Refreshed Data</TableCell>
-            <TableCell>
-              <Checkbox
-                value={refreshData}
-                onChange={handleRefreshChange}
-                id="refreshDataCB"
-                color="primary"
-                inputProps={{ "aria-label": "secondary checkbox" }}
-              />{" "}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={handleButton}
-              >
-                Generate
-              </button>
-            </TableCell>
-          </TableRow>
-        </Table>
-        <label className="text-danger">*Note: Kindly wait for few mins to open PDF file in new windows!.</label>
-      </CardBody>
-    </Card>
-  );
-};
 
 export const API_URL = BASE_URL +"/api/Reports";
 export async function GetReport(FinReportDto) {
@@ -195,6 +63,172 @@ export async function GetAttReport(RequestData) {
     });
     alert("Kindly wait , Data is Downloading....");
 }
+export const FinReportPage = () => {
+  const suhbeader = useSubheader();
+  suhbeader.setTitle("Fin Year Report");
+  return (
+    <>
+     <Table>
+       <TableRow>
+       <TableCell> <RequestCard /></TableCell>
+       <TableCell><AttendaceReportCard/></TableCell>
+      </TableRow>
+      </Table>
+    </>
+  );
+};
+
+export const RequestCard = () => {
+  const curYear = new Date().getFullYear() + 1;
+  const [yearArray, setYearArray] = useState([]);
+  const [finYear, setFinYear] = useState("");
+  const [repoMode, setRepoMode] = useState(2);
+  const [store, setStore] = useState(1);
+  const [startDate,setStartDate] =useState(4); 
+  const [endDate,setEndDate] = useState(3);
+  const [refreshData,setRefreshData] = useState(false);
+  let count = 0;
+  for (let index = 2015; index < curYear; index++) {
+    yearArray[count++] = "" + index + "-" + (index + 1);
+  }
+  const handleYearChange = (event) => {
+    setFinYear(event.target.value);
+    
+  };
+  const handleModeChange = (event) => {
+    setRepoMode(event.target.value);
+  };
+  const handleRefreshChange = (event) => {
+    setRefreshData(event.target.checked);
+  };
+  const handleStoreChange = (event) => {
+    setStore(event.target.value);
+  };
+
+const handleStartDate=(event)=>{
+  setStartDate(event.target.value);
+};
+const handleEndDate=(event)=>{
+  setEndDate(event.target.value);
+};
+
+  const handleButton = (event) => {
+    const yrs = finYear.split("-");
+    const finReq = {
+        storeId: store,
+        startYead: parseInt(yrs[0]),
+        endYear: parseInt(yrs[1]),
+        startMonth: parseInt(startDate),
+        endMonth: parseInt(endDate),
+        mode: parseInt(repoMode),
+        forcedRefresh: refreshData, 
+        isPdf:true
+    };
+    console.log(finReq);
+    GetReport(finReq);
+  };
+
+  return (
+    <Card>
+      <CardHeader title="Financial  Year Report Download">
+        <CardHeaderToolbar></CardHeaderToolbar>
+      </CardHeader>
+      <CardBody>
+      {/* {loading ? <LoadingSpinner /> : <ResultsTable results={data} />} */}
+        <Table>
+          <TableRow>
+            <TableCell className="text-primary">Store</TableCell>
+            
+          </TableRow>
+          <TableRow>
+            <TableCell className="text-danger">Financial Year</TableCell>
+            <TableCell>
+              <Select value={finYear} displayEmpty onChange={handleYearChange} id="finYearSelect">
+              <MenuItem value="" disabled>Select Fin Year</MenuItem>
+                {yearArray.map((item) => (
+                  <MenuItem key={item} value={item}>{item}</MenuItem>
+                ))}
+              </Select>
+            </TableCell>
+            <TableCell className="text-info">Report</TableCell>
+            <TableCell>
+              <Select value={repoMode} onChange={handleModeChange} id="modeSelect">
+                <MenuItem value={1}>Sale Data</MenuItem>
+                <MenuItem value={2}>Cash Book</MenuItem>
+                <MenuItem value={3}>Salary Data</MenuItem>
+                <MenuItem value={4}>Expenses Data</MenuItem>
+                <MenuItem value={5}>Payment Data</MenuItem>
+                <MenuItem value={6}>Receipts Data</MenuItem>
+                <MenuItem value={7}>Bank Data</MenuItem>
+                <MenuItem value={8} disabled>Purchase Data</MenuItem>
+              </Select>
+            </TableCell>
+            <TableCell className="text-danger">Refreshed Data</TableCell>
+            <TableCell>
+              <Checkbox
+                value={refreshData}
+                onChange={handleRefreshChange}
+                id="refreshDataCB"
+                color="primary"
+                inputProps={{ "aria-label": "secondary checkbox" }}
+              />{" "}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="text-danger">Start Month: </TableCell>
+            <TableCell >
+              <Select value={startDate} displayEmpty onChange={handleStartDate} id="startDateSelect">
+                <MenuItem value={1} >Jan</MenuItem>
+                <MenuItem value={2} >Feb</MenuItem>
+                <MenuItem value={3} >March</MenuItem>
+                <MenuItem value={4} >April</MenuItem>
+                <MenuItem value={5} >May</MenuItem>
+                <MenuItem value={6} >June</MenuItem>
+                <MenuItem value={7} >July</MenuItem>
+                <MenuItem value={8} >Aug</MenuItem>
+                <MenuItem value={9} >Sept</MenuItem>
+                <MenuItem value={10} >Oct</MenuItem>
+                <MenuItem value={11} >Nov</MenuItem>
+                <MenuItem value={12} >Dec</MenuItem>
+
+              </Select>
+            </TableCell>
+            <TableCell className="text-danger">End Month:</TableCell>
+            <TableCell >
+              <Select  value={endDate} displayEmpty onChange={handleEndDate} id="endDateSelect">
+                <MenuItem value={1} >Jan</MenuItem>
+                <MenuItem value={2} >Feb</MenuItem>
+                <MenuItem value={3} >March</MenuItem>
+                <MenuItem value={4} >April</MenuItem>
+                <MenuItem value={5} >May</MenuItem>
+                <MenuItem value={6} >June</MenuItem>
+                <MenuItem value={7} >July</MenuItem>
+                <MenuItem value={8} >Aug</MenuItem>
+                <MenuItem value={9} >Sept</MenuItem>
+                <MenuItem value={10} >Oct</MenuItem>
+                <MenuItem value={11} >Nov</MenuItem>
+                <MenuItem value={12} >Dec</MenuItem>
+
+              </Select>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleButton}
+              >
+                Generate
+              </button>
+            </TableCell>
+          </TableRow>
+        </Table>
+        <label className="text-danger">*Note: Kindly wait for few mins to open PDF file in new windows!.</label>
+      </CardBody>
+    </Card>
+  );
+};
 
 export const AttendaceReportCard=()=>{
 
@@ -203,9 +237,10 @@ export const AttendaceReportCard=()=>{
     shallowEqual
   );
   const { totalCount, entities, listLoading } = currentState;
-
+  
   // Employees Redux state
   const dispatch = useDispatch();
+  
   useEffect(() => {
     // clear selections list
     
@@ -220,14 +255,16 @@ export const AttendaceReportCard=()=>{
   const [repoMode, setRepoMode] = useState(2);
   const [store, setStore] = useState(1);
   const [refreshData,setRefreshData] = useState(false);
+  const [finYear, setFinYear] = useState("");
+  const[forMonth,setForMonth] = useState(0);
+  const handleYearChange = (event) => {setFinYear(event.target.value); };
+  const handleForMonth = (event) => {setForMonth(event.target.value); };
   let count = 0;
   for (let index = 2015; index < curYear; index++) {
     yearArray[count++] = "" + index + "-" + (index + 1);
   }
   const handleEmpChange = (event) => {
     setEmp(event.target.value);
-    console.log(emp); 
-    console.log(event.target.value);
   };
   const handleModeChange = (event) => {
     setRepoMode(event.target.value);
@@ -242,6 +279,8 @@ export const AttendaceReportCard=()=>{
     const ReqData={
       storeId:store, 
       employeeId:emp,
+      finYear:finYear,
+      month:parseInt(forMonth),
       forcedRefresh: refreshData
     };
     GetAttReport(ReqData);
@@ -282,6 +321,35 @@ export const AttendaceReportCard=()=>{
                 color="primary"
                 inputProps={{ "aria-label": "secondary checkbox" }}
               />{" "}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+          <TableCell className="text-danger">Financial Year</TableCell>
+            <TableCell>
+              <Select value={finYear} displayEmpty onChange={handleYearChange} id="finYearSelect">
+              <MenuItem value="" disabled>Select Fin Year</MenuItem>
+                {yearArray.map((item) => (
+                  <MenuItem key={item} value={item}>{item}</MenuItem>
+                ))}
+              </Select>
+            </TableCell>
+            <TableCell className="text-danger">Start Month: </TableCell>
+            <TableCell >
+              <Select value={forMonth} displayEmpty onChange={handleForMonth} id="forMonthSelect">
+              <MenuItem value={0} >All</MenuItem>
+                <MenuItem value={4} >April</MenuItem>
+                <MenuItem value={5} >May</MenuItem>
+                <MenuItem value={6} >June</MenuItem>
+                <MenuItem value={7} >July</MenuItem>
+                <MenuItem value={8} >Aug</MenuItem>
+                <MenuItem value={9} >Sept</MenuItem>
+                <MenuItem value={10} >Oct</MenuItem>
+                <MenuItem value={11} >Nov</MenuItem>
+                <MenuItem value={12} >Dec</MenuItem>
+                <MenuItem value={1} >Jan</MenuItem>
+                <MenuItem value={2} >Feb</MenuItem>
+                <MenuItem value={3} >March</MenuItem>
+              </Select>
             </TableCell>
           </TableRow>
           <TableRow>
