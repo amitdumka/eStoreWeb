568fa4dee9e640864d780b9f8fb4356fce5ad1d7
Amit Kumar <amitnarayansah@gmail.com>
2021-08-19 00:46:30 +0530

    PersonalExpenses Db

---
+++ eStore.Database/Migrations/20210818190921_PersonalExp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Database.Migrations
+{
+    public partial class PersonalExp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PersonalExpenses",
+                columns: table => new
+                {
+                    PersonalExpenseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Particulars = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsPrivate = table.Column<bool>(type: "bit", nullable: false),
+                    PaymentMode = table.Column<int>(type: "int", nullable: false),
+                    BankAccountId = table.Column<int>(type: "int", nullable: true),
+                    BankDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InvoiceNo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsWithInvoice = table.Column<bool>(type: "bit", nullable: false),
+                    URL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsReadOnly = table.Column<bool>(type: "bit", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalExpenses", x => x.PersonalExpenseId);
+                    table.ForeignKey(
+                        name: "FK_PersonalExpenses_BankAccounts_BankAccountId",
+                        column: x => x.BankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "BankAccountId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalExpenses_BankAccountId",
+                table: "PersonalExpenses",
+                column: "BankAccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonalExpenses");
+        }
+    }
+}
