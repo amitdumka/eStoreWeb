475b65b62021614a78448d807a724da461511471
Amit Kumar <amitnarayansah@gmail.com>
2021-10-02 13:35:44 +0530

    published

---
+++ eStore.Database/Migrations/20211002075430_stocklist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Database.Migrations
+{
+    public partial class stocklist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockLists",
+                columns: table => new
+                {
+                    StockListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stock = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    LastAccess = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockLists", x => x.StockListId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockLists");
+        }
+    }
+}
