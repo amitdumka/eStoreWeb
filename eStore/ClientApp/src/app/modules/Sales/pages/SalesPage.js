a3cf02bb214ca03af0d0fc8ab35b2c3f728e8f40
Amit Kumar <amitnarayansah@gmail.com>
2022-02-25 00:59:16 +0530

    package

--- eStore/ClientApp/src/app/modules/Sales/pages/SalesPage.js
+++ eStore/ClientApp/src/app/modules/Sales/pages/SalesPage.js
@@ -2,10 +2,10 @@ import React, { Suspense } from "react";
 import { Redirect, Switch } from "react-router-dom";
 import { LayoutSplashScreen, ContentRoute } from "../../../../_metronic/layout";
 import { DailySalesPage } from "./DailySales/DailySalesPage";
-import { InvoicesPage } from "./Invoices/InvoicesPage";
-import { ManualInvoicesPage } from "./ManualInvoices/ManualInvoicesPage";
-import { InvoiceEditorPage } from "./Invoices/dialogs/Edit/InvoiceEditorPage";
-import { EditPage as ManualEditPage } from "./ManualInvoices/Dialogs/Edit/EditPage";
+//import { InvoicesPage } from "./Invoices/InvoicesPage";
+//import { ManualInvoicesPage } from "./ManualInvoices/ManualInvoicesPage";
+//import { InvoiceEditorPage } from "./Invoices/dialogs/Edit/InvoiceEditorPage";
+//import { EditPage as ManualEditPage } from "./ManualInvoices/Dialogs/Edit/EditPage";
 
 export default function SalesPage() {
   return (
@@ -17,20 +17,20 @@ export default function SalesPage() {
         }
         <ContentRoute path="/sales/dailySales" component={DailySalesPage} />
 
-        <ContentRoute
+        {/* <ContentRoute
           path="/sales/invoices/new"
           component={InvoiceEditorPage}
-        />
-        <ContentRoute
+        /> */}
+        {/* <ContentRoute
           path="/sales/manualInvoices/new"
           component={ManualEditPage}
-        />
+        /> */}
 
-        <ContentRoute path="/sales/invoices" component={InvoicesPage} />
-        <ContentRoute
+        {/* <ContentRoute path="/sales/invoices" component={InvoicesPage} /> */}
+        {/* <ContentRoute
           path="/sales/manualInvoices"
           component={ManualInvoicesPage}
-        />
+        /> */}
       </Switch>
     </Suspense>
   );
