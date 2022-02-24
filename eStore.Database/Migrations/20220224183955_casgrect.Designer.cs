f353a8c65da09221b33e9f47b4d4b8ca01abd403
Amit Kumar <amitnarayansah@gmail.com>
2022-02-25 00:47:57 +0530

    migration

--- eStore.Database/Migrations/20220224183955_casgrect.Designer.cs
+++ eStore.Database/Migrations/20220224183955_casgrect.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eStore.Database;
 
 namespace eStore.Database.Migrations
 {
     [DbContext(typeof(eStoreDbContext))]
-    partial class eStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220224183955_casgrect")]
+    partial class casgrect
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ eStore.Database/Migrations/20220224183955_casgrect.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Database.Migrations
+{
+    public partial class casgrect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DueRecoverds_DuesLists_DuesListId",
+                table: "DueRecoverds");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DueRecoverds_Stores_StoreId",
+                table: "DueRecoverds");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DueRecoverds",
+                table: "DueRecoverds");
+
+            migrationBuilder.RenameTable(
+                name: "DueRecoverds",
+                newName: "DueRecovereds");
+
+            migrationBuilder.RenameColumn(
+                name: "RecieptSlipNo",
+                table: "Receipts",
+                newName: "ReceiptSlipNo");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DueRecoverds_StoreId",
+                table: "DueRecovereds",
+                newName: "IX_DueRecovereds_StoreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DueRecoverds_DuesListId",
+                table: "DueRecovereds",
+                newName: "IX_DueRecovereds_DuesListId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DueRecovereds",
+                table: "DueRecovereds",
+                column: "DueRecoverdId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DueRecovereds_DuesLists_DuesListId",
+                table: "DueRecovereds",
+                column: "DuesListId",
+                principalTable: "DuesLists",
+                principalColumn: "DuesListId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DueRecovereds_Stores_StoreId",
+                table: "DueRecovereds",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "StoreId",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DueRecovereds_DuesLists_DuesListId",
+                table: "DueRecovereds");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DueRecovereds_Stores_StoreId",
+                table: "DueRecovereds");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DueRecovereds",
+                table: "DueRecovereds");
+
+            migrationBuilder.RenameTable(
+                name: "DueRecovereds",
+                newName: "DueRecoverds");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceiptSlipNo",
+                table: "Receipts",
+                newName: "RecieptSlipNo");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DueRecovereds_StoreId",
+                table: "DueRecoverds",
+                newName: "IX_DueRecoverds_StoreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_DueRecovereds_DuesListId",
+                table: "DueRecoverds",
+                newName: "IX_DueRecoverds_DuesListId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DueRecoverds",
+                table: "DueRecoverds",
+                column: "DueRecoverdId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DueRecoverds_DuesLists_DuesListId",
+                table: "DueRecoverds",
+                column: "DuesListId",
+                principalTable: "DuesLists",
+                principalColumn: "DuesListId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DueRecoverds_Stores_StoreId",
+                table: "DueRecoverds",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "StoreId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
