50ef5c8703a215ab1c31d695d18aa5eeeb86c399
Amit Kumar <amitnarayansah@gmail.com>
2022-03-06 11:21:50 +0530

    publish ready

---
+++ eStore.Database/Migrations/20220306054923_MonthlyAtt.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Database.Migrations
+{
+    public partial class MonthlyAtt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MonthlyAttendances",
+                columns: table => new
+                {
+                    MonthlyAttendanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EntryStatus = table.Column<int>(type: "int", nullable: false),
+                    IsReadOnly = table.Column<bool>(type: "bit", nullable: false),
+                    StoreId = table.Column<int>(type: "int", nullable: false),
+                    OnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Present = table.Column<int>(type: "int", nullable: false),
+                    HalfDay = table.Column<int>(type: "int", nullable: false),
+                    Sunday = table.Column<int>(type: "int", nullable: false),
+                    PaidLeave = table.Column<int>(type: "int", nullable: false),
+                    CasualLeave = table.Column<int>(type: "int", nullable: false),
+                    Absent = table.Column<int>(type: "int", nullable: false),
+                    Holidays = table.Column<int>(type: "int", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoOfWorkingDays = table.Column<int>(type: "int", nullable: false),
+                    BillableDays = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonthlyAttendances", x => x.MonthlyAttendanceId);
+                    table.ForeignKey(
+                        name: "FK_MonthlyAttendances_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MonthlyAttendances_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "YearlyAttendances",
+                columns: table => new
+                {
+                    YearlyAttendanceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EntryStatus = table.Column<int>(type: "int", nullable: false),
+                    IsReadOnly = table.Column<bool>(type: "bit", nullable: false),
+                    StoreId = table.Column<int>(type: "int", nullable: false),
+                    OnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Present = table.Column<int>(type: "int", nullable: false),
+                    HalfDay = table.Column<int>(type: "int", nullable: false),
+                    Sunday = table.Column<int>(type: "int", nullable: false),
+                    PaidLeave = table.Column<int>(type: "int", nullable: false),
+                    CasualLeave = table.Column<int>(type: "int", nullable: false),
+                    Absent = table.Column<int>(type: "int", nullable: false),
+                    Holidays = table.Column<int>(type: "int", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoOfWorkingDays = table.Column<int>(type: "int", nullable: false),
+                    BillableDays = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YearlyAttendances", x => x.YearlyAttendanceId);
+                    table.ForeignKey(
+                        name: "FK_YearlyAttendances_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_YearlyAttendances_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyAttendances_EmployeeId",
+                table: "MonthlyAttendances",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyAttendances_StoreId",
+                table: "MonthlyAttendances",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_YearlyAttendances_EmployeeId",
+                table: "YearlyAttendances",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_YearlyAttendances_StoreId",
+                table: "YearlyAttendances",
+                column: "StoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MonthlyAttendances");
+
+            migrationBuilder.DropTable(
+                name: "YearlyAttendances");
+        }
+    }
+}
